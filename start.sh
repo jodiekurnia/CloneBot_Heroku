c42b0b7e8740c7e7a636f9f2b6f75323552bf5df
Jodie Kurnia <54063383+jodiekurnia@users.noreply.github.com>
2022-06-18 14:26:20 +0700

    better start script

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
-echo "[General]
-path_to_gclone = ./gclone
-telegram_token = $telegram_token
-user_ids = $user_ids
-group_ids = $group_ids
-gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
+FILE=telegram_gcloner/config.ini
+if [ ! -f "$FILE" ]; then
+    echo "[General]
+    path_to_gclone = ./gclone
+    telegram_token = $telegram_token
+    user_ids = $user_ids
+    group_ids = $group_ids
+    gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
+fi
 npm install http-server -g
 http-server -p $PORT &
 python3 telegram_gcloner/telegram_gcloner.py
